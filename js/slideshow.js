727ca3ae674325ff932359148b5e213670bc4a28
Justina Jemeljanovaite <just1528@stud.kea.dk>
2021-03-23 12:36:37 +0200

    improved JS

--- js/slideshow.js
+++ js/slideshow.js
@@ -6,17 +6,18 @@ function slideText() {
  }
 
 // slideshow
-var slideIndex = 0;
+// tutorial from https://www.w3schools.com/w3css/w3css_slideshow.asp
+let slideIndex = 0;
 carousel();
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("slides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  let imageRow;
+  const image = document.querySelectorAll(".slides");
+  for (imageRow = 0; imageRow < image.length; imageRow++) {
+    image[imageRow].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
+  if (slideIndex > image.length) {slideIndex = 1}
+  image[slideIndex-1].style.display = "block";
   setTimeout(carousel, 800); // Change image every 0.8s
 }
